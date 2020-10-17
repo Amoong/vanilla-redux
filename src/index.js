2948a521ed17e446cffcf33b76c50518d0748f86
moo1323@naver.com <moo1323@naver.com>
2020-10-17 09:14:48 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { Provider } from "react-redux";
-import store from "./store";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root"),
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+
+console.log(countStore.getState());
